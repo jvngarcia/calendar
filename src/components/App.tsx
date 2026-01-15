1b559cfc37a74f25acf83148fc62da26f87a1f8d
jvngarcia <angelgarciaweb@gmail.com>
2026-01-15 09:21:44 -0400

    feat(footer): add footer

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Header';
 import Calendar from './Calendar';
+import Footer from './Footer';
 
 import { useCalendarStore } from '../lib/store';
 
@@ -22,6 +23,7 @@ export default function App() {
             <main className="flex-grow w-full max-w-[1400px] mx-auto px-4 sm:px-6 lg:px-8 py-8 print:p-0 print:max-w-none print:w-full relative">
                 <Calendar />
             </main>
+            <Footer />
         </div>
     );
 }
